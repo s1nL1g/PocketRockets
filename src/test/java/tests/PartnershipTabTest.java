3aab41c23c298cf3714b803b6930d224cf9c124b
Bogdan <bodaginaylo33@gmail.com>
2023-05-23 05:01:24 +0300

    Made Test Case 8 - "Verify if the partnership tab is functioning correctly"

---
+++ src/test/java/tests/PartnershipTabTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import base.AbstractBaseTest;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.PartnershipTabPage;
+
+import static org.testng.Assert.assertTrue;
+
+public class PartnershipTabTest extends AbstractBaseTest {
+    @Test
+    public void EpicentrTest() {
+        PartnershipTabPage partnershipTabPage = new PartnershipTabPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        partnershipTabPage.clickBecomeAPartner()
+                .clickBuildMaterials()
+                .hoverCursorToGoBtn()
+                .waitUntilBeVisible()
+                .clickGoEpicentrBtn();
+
+        homePage.goToLastWindow();
+
+        assertTrue(partnershipTabPage.getEpicentrLogoBtn().isDisplayed());
+    }
+}
