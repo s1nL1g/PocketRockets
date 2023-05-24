53ccf52c3aefe0e8585d40b469941ef0f5e24bfc
Bogdan <bodaginaylo33@gmail.com>
2023-05-24 13:09:17 +0300

    Added empty fields

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,9 +4,11 @@ import base.AbstractBaseTest;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
+
 import static org.testng.Assert.assertTrue;
 
 public class LoginTests extends AbstractBaseTest {
+
     @Test
     public void loginPositiveTest() {
         LoginPage loginPage = new LoginPage(driver);
@@ -16,6 +18,7 @@ public class LoginTests extends AbstractBaseTest {
         loginPage.setEmailField()
                 .setPassword()
                 .clickBtnEnter();
+
         assertTrue(loginPage.getMyAccountBtn().isDisplayed());
     }
 }
