0a6caff4da27604800abaccd8df5e47f23a17f04
Bogdan <bodaginaylo33@gmail.com>
2023-05-24 13:22:11 +0300

    Copied all changes from origin/dev

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -18,13 +18,13 @@ public class HomePage extends AbstractBasePage {
     private static final String BTN_GIFT_CLOSE = "//div[@class='gift-close']";
     private static final String BTN_ENTER = "//label[@class='login-block__login-btn']";
     private static final String INPUT_TRACKING = "//input[@class='tracking__input']";
+    private static final String INPUT_SEARCH = "//input[@class='main-search__input']";
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
     public WebElement getLogo() {
-
         return driver.findElement(By.xpath(BTN_LOGO));
     }
 
@@ -64,6 +64,10 @@ public class HomePage extends AbstractBasePage {
 
     public WebElement getInputTracking() {
         return driver.findElement(By.xpath(INPUT_TRACKING));
+    }
+
+    public WebElement getInputSearch() {
+        return driver.findElement(By.xpath(INPUT_SEARCH));
     }
 
     public HomePage goToLastWindow() {
